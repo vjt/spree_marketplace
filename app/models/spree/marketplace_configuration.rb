b2367d3bad917eb2d88f560092976e90a89e88cd
fir <firman.alqoumy@gmail.com>
2015-04-06 17:58:55 +0700

    remove rails_env from mar_configuration

--- app/models/spree/marketplace_configuration.rb
+++ app/models/spree/marketplace_configuration.rb
@@ -8,7 +8,7 @@ module Spree
     preference :stripe_publishable_key, :string, default: -> {
       if ActiveRecord::Base.connection.table_exists?(:spree_payment_methods)
         # If you are using Stripe as a credit card processor we automatically lookup your api key to use for payments.
-        Spree::PaymentMethod.where(type: 'Spree::Gateway::StripeGateway', environment: Rails.env).first.try(:preferred_publishable_key)
+        Spree::PaymentMethod.where(type: 'Spree::Gateway::StripeGateway').first.try(:preferred_publishable_key)
       else
         nil
       end
@@ -17,7 +17,7 @@ module Spree
     preference :stripe_secret_key, :string, default: -> {
       if ActiveRecord::Base.connection.table_exists?(:spree_payment_methods)
         # If you are using Stripe as a credit card processor we automatically lookup your api key to use for payments.
-        Spree::PaymentMethod.where(type: 'Spree::Gateway::StripeGateway', environment: Rails.env).first.try(:preferred_secret_key)
+        Spree::PaymentMethod.where(type: 'Spree::Gateway::StripeGateway').first.try(:preferred_secret_key)
       else
         nil
       end
