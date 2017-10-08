aa4acd1b6f677bdd9954301632922a51c31cfa32
Marcello Barnaba <vjt@openssl.it>
2017-10-08 22:17:36 +0200

    Whitespace

--- app/overrides/add_supplier_related_products.rb
+++ app/overrides/add_supplier_related_products.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-	:virtual_path => "spree/products/show",
-  :name => "add_supplier_related_products",
-	:insert_bottom => '[data-hook="product_show"]',
-  :partial => "spree/products/supplier_products"
+  :virtual_path  => "spree/products/show",
+  :name          => "add_supplier_related_products",
+  :insert_bottom => '[data-hook="product_show"]',
+  :partial       => "spree/products/supplier_products"
 )
