d0a440d05825f8ec9da1f63fa437eba8789150b9
Marcello Barnaba <vjt@openssl.it>
2017-10-08 22:07:20 +0200

    Reduce supplier ability to only edit the stock of its products

--- app/models/spree/supplier_ability.rb
+++ app/models/spree/supplier_ability.rb
@@ -13,10 +13,6 @@ module Spree
           end
           can :create, Spree::Digital
         end
-        can [:admin, :manage], Spree::Image do |image|
-          image.viewable.product.supplier_ids.include?(user.supplier_id)
-        end
-        can :create, Spree::Image
         if SpreeMarketplace::Engine.spree_group_price_available?
           # can [:admin, :manage], Spree::GroupPrice, variant: { supplier_ids: user.supplier_id }
           can [:admin, :manage], Spree::GroupPrice do |price|
@@ -29,19 +25,11 @@ module Spree
             relation.relatable.supplier_ids.include?(user.supplier_id)
           end
         end
-        # TODO: Want this to be inline like:
-        # can [:admin, :manage], Spree::Product, suppliers: { id: user.supplier_id }
-        can [:admin, :manage], Spree::Product do |product|
+        can [:admin], Spree::Stock
+        can [:stock], Spree::Product do |product|
           product.supplier_ids.include?(user.supplier_id)
         end
-        can [:admin], Spree::Stock
-        can [:admin, :create, :index], Spree::Product
-        # can [:admin, :manage], Spree::ProductProperty, product: { supplier_ids: user.supplier_id }
-        can [:admin, :manage], Spree::ProductProperty do |property|
-          property.product.supplier_ids.include?(user.supplier_id)
-        end
-        can [:admin, :index, :read], Spree::Property
-        can [:admin, :read], Spree::Prototype
+        can [:admin, :index], Spree::Product
         can [:admin, :manage, :read, :ready, :ship], Spree::Shipment, order: { state: 'complete' }, stock_location: { supplier_id: user.supplier_id }
         can [:admin, :create, :update], :stock_items
         can [:admin, :manage], Spree::StockItem, stock_location_id: user.supplier.stock_locations.pluck(:id)
@@ -50,12 +38,6 @@ module Spree
         can [:admin, :manage], Spree::StockMovement, stock_item: { stock_location_id: user.supplier.stock_locations.pluck(:id) }
         can :create, Spree::StockMovement
         can [:admin, :update], Spree::Supplier, id: user.supplier_id
-        # TODO: Want this to be inline like:
-        # can [:admin, :manage], Spree::Variant, supplier_ids: user.supplier_id
-        can [:admin, :create, :index], Spree::Variant
-        can [:admin, :manage], Spree::Variant do |variant|
-          variant.supplier_ids.include?(user.supplier_id)
-        end
       end
 
       if SpreeMarketplace::Config[:allow_signup]
